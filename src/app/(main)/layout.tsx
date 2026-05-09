2bf3d3805f543964c1c1ce331a7e3282d1fd18b0
ryota-yamamoto-03 <ryota.yamamoto03@gmail.com>
2026-05-10 02:29:02 +0900

    Remove max-width constraint from main layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,9 +4,7 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
   return (
     <div className="flex min-h-screen bg-gray-50">
       <Sidebar />
-      <main className="flex-1 ml-64 p-8 max-w-4xl">
-        {children}
-      </main>
+      <main className="flex-1 ml-64 p-8">{children}</main>
     </div>
   )
 }
